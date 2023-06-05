2bcc3eca6fff12646cded31e227e3b60c3eebdc2
harshawije-dev <harsha.bro1997@gmail.com>
2023-06-05 17:22:35 +0530

    app init updates

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="footer-base">
+      <p className="text-bottom">Powered by Candy Factory Group</p>
+    </div>
+  );
+};
+
+export default Footer;
